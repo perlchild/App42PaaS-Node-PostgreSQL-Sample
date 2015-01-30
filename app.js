ce71661ffd1591f2c84c22c6bc2e215e722b4957
Eric Robibaro <eric.robibaro@radialpoint.com>
2015-01-30 11:07:34 -0500

    almost working copy

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
  * Module dependencies.
  */
 
+
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
@@ -10,6 +11,8 @@ var http = require('http');
 var path = require('path');
 var pg   = require('pg');
 var app  = express();
+var bugsnag = require("bugsnag");
+bugsnag.register("093309e1989fb679bbe95994eff4ac47");
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -22,41 +25,43 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bugsnag.requestHandler);
 
 // development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+//if ('development' == app.get('env')) {
+//  app.use(express.errorHandler());
+//}
 
 // Database URL and connection Object
-var conString = "postgres://User Name:Password@VM IP:VM PORT/DATABASE NAME";
+var conString = "postgres://postgres:E_Dba007@localhost:5432/espresso";
 // var conString = "postgres://postgres:ankit1234@localhost:5432/nodedb_development";
 var connection = new pg.Client(conString);
 // Connect PostgreSQL
 connection.connect(function(err) {
   if(err) {
-    return console.error('could not connect to postgres', err);
+ //   return console.error('could not connect to postgres', err);
+      bugsnag.notify(new Error("Fatal"),  err);
   }
 });
 
 // Database setup
 // connection.query('DROP TABLE users');
-connection.query("CREATE TABLE IF NOT EXISTS users(id integer, name varchar(128), email varchar(128), des varchar(256))");
+connection.query("CREATE TABLE IF NOT EXISTS users(id integer, first_name varchar(128), last_name varchar(128), email varchar(128))");
 
 // Add a new User
 app.get("/users/new", function (req, res) {
   res.render("new", {
-    title: 'App42PaaS Express PostgreSQL Application'
+    title: 'Hackathon Reggaethon Express PostgreSQL Application'
   });
 });
 
 // Save the Newly created User
 app.post("/users", function (req, res) {
-  var name=req.body.name;
+  var first_name=req.body.first_name;
+  var last_name=req.body.last_name;
   var email=req.body.email;
-  var des=req.body.des;
   
-  connection.query('INSERT INTO users (name,email,des) VALUES ($1,$2,$3) RETURNING id', [name, email, des], function(err, docs) {
+  connection.query('INSERT INTO users (first_name,last_name,email) VALUES ($1,$2,$3) RETURNING id', [first_name, last_name, email], function(err, docs) {
     res.redirect('/');
   });
 });
@@ -69,6 +74,6 @@ http.createServer(app).listen(app.get('port'), function(){
 // App root
 app.get('/', function(req, res){
   connection.query('SELECT * FROM users', function(err, docs) { 
-    res.render('users', {users: docs, title: 'App42PaaS Express PostgreSQL Application'});
+    res.render('users', {users: docs, title: 'Hackathon Express PostgreSQL Application'});
   });
 });
